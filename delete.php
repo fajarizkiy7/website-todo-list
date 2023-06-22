6d8ef615a44d24934f383044f8cec28c8282a50e
Fajar Rizki Yunanto <97289905+fajarizkiy7@users.noreply.github.com>
2023-06-22 18:33:32 +0700

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+require_once "db.php";
+if (isset($_GET['id'])) {
+	$id = base64_decode($_GET['id']);
+	$delete_query = "DELETE FROM task_table WHERE id=$id";
+	$delete_query;
+	$run_query = $dbcon->query($delete_query);
+	if($run_query){
+		$_SESSION['delete_success'] = "Tugas berhasil dihapus!";
+	}
+	header('location: index.php');
+}
+else{
+	header('location: index.php');
+}
+?>
